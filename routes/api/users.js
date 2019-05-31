8ec1ed1663195c97a874555e21382fd24a9a7128
Besart Marku <b.marku@evolutivo.it>
2019-05-31 14:22:17 +0200

    Implementing JWT

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator/check");
 
 const User = require("../../models/Users");
@@ -14,8 +16,10 @@ router.post(
     check("name", "Name is required")
       .not()
       .isEmpty(),
-    check("email", "email not valid").isEmail(),
-    check("password", "enter valid password").isLength({ min: 6 })
+    check("email", "Put a valid Email").isEmail(),
+    check("password", "Password must be at least 6 characters").isLength({
+      min: 6
+    })
   ],
   async (req, res) => {
     const errors = validationResult(req);
@@ -52,7 +56,22 @@ router.post(
       await user.save();
 
       //Return jsonwebtoken
-      res.send("User Registered");
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send(" Server Error");
