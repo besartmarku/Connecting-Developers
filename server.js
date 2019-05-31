6fe731a9e9bfc65c574eaa54bcb2037da13672a3
Besart Marku <b.marku@evolutivo.it>
2019-05-31 11:57:05 +0200

    Configure conecction with MongoDB

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
-
+//Connect db
+connectDB();
 app.get("/", (req, res) => res.send("API Running"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => console.log(`Server started at ${PORT}`));
