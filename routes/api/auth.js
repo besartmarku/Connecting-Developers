6180bf1037e15c295e32ca2a4d1502df95f0779e
Besart Marku <b.marku@evolutivo.it>
2019-06-03 12:14:33 +0200

    Create profile model and create/update profile routes

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -27,7 +27,6 @@ router.post(
 
     try {
       //See if user exists
-
       let user = await User.findOne({ email });
       if (!user) {
         res.status(400).json({ errors: [{ msg: "Invalid Credentials" }] });
