1325d5cf1590ff6991d4d6c2c24014f7ee98dee6
Besart Marku <b.marku@evolutivo.it>
2019-06-13 11:43:50 +0200

    New Components and minor changes, fixes

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,35 +1,38 @@
-import React from "react";
-import { connect } from "react-redux";
-import PropTypes from "prop-types";
-import { Link, Redirect } from "react-router-dom";
+import React from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 const Landing = ({ isAuthenticated }) => {
   if (isAuthenticated) {
-    return <Redirect to="/dashboard" />;
+    return <Redirect to='/dashboard' />;
   }
+
   return (
-    <div>
-      <section className="landing">
-        <div className="dark-overlay">
-          <div className="landing-inner">
-            <h1 className="x-large">Developer Connector</h1>
-            <p className="lead">
-              Create a developer profile/portfolio, share posts and get help
-              from other developers
-            </p>
-            <div className="buttons">
-              <Link to="register" className="btn btn-primary">
-                Sign Up
-              </Link>
-              <Link to="login" className="btn btn-light">
-                Login
-              </Link>
-            </div>
+    <section className='landing'>
+      <div className='dark-overlay'>
+        <div className='landing-inner'>
+          <h1 className='x-large'>Developer Connector</h1>
+          <p className='lead'>
+            Create a developer profile/portfolio, share posts and get help from
+            other developers
+          </p>
+          <div className='buttons'>
+            <Link to='/register' className='btn btn-primary'>
+              Sign Up
+            </Link>
+            <Link to='/login' className='btn btn-light'>
+              Login
+            </Link>
           </div>
         </div>
-      </section>
-    </div>
+      </div>
+    </section>
   );
+};
+
+Landing.propTypes = {
+  isAuthenticated: PropTypes.bool
 };
 
 const mapStateToProps = state => ({
